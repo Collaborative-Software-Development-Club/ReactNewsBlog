dfe193a00c3ce46256b6771e94ec4a079aac055c
Phaskett <parkerhaskett@gmail.com>
2024-02-13 20:32:33 -0500

    Worked on createpost.jsx file. Define structure in for Blogpost in html. Created a createBlogPost component.

--- react-news-blog/src/layouts/MainPage/CreatePost.jsx
+++ react-news-blog/src/layouts/MainPage/CreatePost.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const CreatePost = ({uploadPost}) => {
+//Create the structure of the blog post
+const CreatePost = ({ title, content }) => {
   return (
-    <div>CreatePost component (test)</div>
-  )
+    <div className = "blog-Post">
+      <h2>{title}</h2>
+      <div className = "blog-post-content">{content}</div>
+    </div>
+  );
+}
+
+//pass in the content of the blog post with props
+function CreateBlogPost({title, content}){
+  return(
+    <div className = "blog-Post-Stream">
+      <CreatePost title = {title} content = {content}/>
+    </div>
+  );
 }
 
 export default CreatePost
