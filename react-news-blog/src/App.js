e6a9a96a80b6cf4f53a28062fc0c1b3b5df2d7e2
Phaskett <parkerhaskett@gmail.com>
2024-03-06 20:19:01 -0500

    Messed around with react router components and tried to navigate to index.jsx in layouts

--- react-news-blog/src/App.js
+++ react-news-blog/src/App.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 import './App.css';
 import MainPage from './layouts/MainPage'
+import Postspage from './layouts/Posts'
+//import react router to set up routing map
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <MainPage />
+    <>
+      <BrowserRouter>
+        <Routes path="/">
+          <Route path = "post" element ={<Postspage/>}/>
+          <Route index element ={<MainPage/>}/>
+
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
