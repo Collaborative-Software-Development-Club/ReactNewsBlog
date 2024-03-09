4ddbb9f8c03fd9fd81e66a3efd2ef577728687b1
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-03-09 14:09:51 -0500

    changed like button to show like status

--- react-news-blog/src/layouts/MainPage/LikeCount.jsx
+++ react-news-blog/src/layouts/MainPage/LikeCount.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-const LikeCount = ({likeCount}) => {
+const LikeCount = ({ likeCount }) => {
     console.log(likeCount)
     const [isLiked, setLikes] = useState(false)
 
-	const toggleLike = () => {
-        setLikes((prev) => !prev);
+    const toggleLike = () => {
+        setLikes((prev) => !prev)
     }
 
     return (
-        <div className="like-counter">
-            <button className="like-Button" onClick={toggleLike}>Like</button> 
-            <p className="likes">{isLiked ? likeCount + 1: likeCount}</p>
+        <div className='like-counter'>
+            <button className='like-Button' onClick={toggleLike}>
+                {isLiked ? 'unlike' : 'like'}
+            </button>
+            <p className='likes'>{isLiked ? likeCount + 1 : likeCount}</p>
         </div>
-    );
+    )
+}
 
-};
-
-export default LikeCount;
+export default LikeCount
