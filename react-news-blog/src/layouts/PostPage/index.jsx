0a2d80bb54c9619b77a3efd7b1d980bbc7efd426
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-03-09 13:36:31 -0500

    added place for comments in post page

--- react-news-blog/src/layouts/PostPage/index.jsx
+++ react-news-blog/src/layouts/PostPage/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 import BlogPost from "../../components/BlogPost";
 
-import usePostData from "../../data/usePostData"
+import usePostData from "../../data/usePostData";
 
-const PostPage = ()=> {
-    const { id } = useParams();
-    const postData = usePostData(id);
-    return <BlogPost postData={postData} />
-}
+const PostPage = () => {
+	const { id } = useParams();
+	const postData = usePostData(id);
+	return (
+		<>
+			<BlogPost postData={postData} />
+            <h2>Comments:</h2>
+            <ul><li>Add comments here</li></ul>
+		</>
+	);
+};
 
 export default PostPage;
