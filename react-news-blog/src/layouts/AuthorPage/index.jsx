fa78360b23be3f48429a77d7259a7c65bc87f389
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-03-21 15:11:18 -0400

    final adjustments to author

--- react-news-blog/src/layouts/AuthorPage/index.jsx
+++ react-news-blog/src/layouts/AuthorPage/index.jsx
@@ -1,45 +1,38 @@
 import React from 'react'
+
 import PostPreview from '../../components/PostPreview'
 
-import useUserData from '../../data/useUserData.js'
-
-import {useParams} from 'react-router-dom'
-
+import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 const AuthorPage = ({ posts, uploadPost }) => {
-    const user = useUserData()
     const { author } = useParams()
+    const parsedAuthor = parseURL(author)
+    const filteredPosts = posts.filter((postData) => postData.author === parsedAuthor)
 
-    console.log('main page', posts)
-
-	const filteredPosts = posts.filter((postData) => postData.author === author)
-
-	
-        return (
-			<>
-			
-				<h1>{author}</h1>
-
-				{filteredPosts.length === 0 ? <h1>No Post Found</h1> : 
-	
-				<div>
-					<ul className='blog-Post-Stream'>
-						{filteredPosts.map((post) => (
-							<li>
-								<Link to={`./post/${post.author}`}>
-									<PostPreview postData={post} key={post.id} />
-								</Link>
-							</li>
-						))}
-					</ul>
-				</div>
-}
-			</>
-		)
-    
-
-    
+    return (
+        <>
+            <h1>{author}</h1>
+            <h2>Posts</h2>
+            {filteredPosts.length === 0 ? (
+                <p>No Post Found</p>
+            ) : (
+                <ul className='blog-Post-Stream'>
+                    {filteredPosts.map((post) => (
+                        <li>
+                            <Link to={`/post/${post.id}`}>
+                                <PostPreview postData={post} key={post.id} />
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </>
+    )
 }
 
 export default AuthorPage
+
+function parseURL(url) {
+    return url.replace('%20', ' ')
+}
--- react-news-blog/src/layouts/MainPage/index.jsx
+++ react-news-blog/src/layouts/MainPage/index.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import PostWriter from "./PostWriter.jsx";
-import PostPreview from "../../components/PostPreview.jsx";
+import React from 'react'
+import PostWriter from './PostWriter.jsx'
+import PostPreview from '../../components/PostPreview.jsx'
 
-import useUserData from "../../data/useUserData.js"
+import useUserData from '../../data/useUserData.js'
 
-import {Link} from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 //why do we have an index.jsx file and an index.js file??
-const MainPage = ({posts, uploadPost}) => {
-    const user = useUserData();
-	console.log("main page", posts);
+const MainPage = ({ posts, uploadPost }) => {
+    const user = useUserData()
+    console.log('main page', posts)
 
-	return (
-		<>
-			<h1>CSDC Blog</h1>
-			<div className="main-page-content">
-				<div>
-					<h2>Feed</h2>
-					<ul className="blog-Post-Stream">
-				{posts.map((post) => (
-					<li><Link to={`./post/${post.id}`}><PostPreview postData={post} key={post.id} /></Link></li>
-				
-				))}
-			</ul>
-				</div>
-				<PostWriter uploadPost={uploadPost} user={user} />
-			</div>
-		</>
-	);
-};
+    return (
+        <>
+            <h1>CSDC Blog</h1>
+            <div className='main-page-content'>
+                <div>
+                    <h2>Feed</h2>
+                    <ul className='blog-Post-Stream'>
+                        {posts.map((post) => (
+                            <li>
+                                <Link to={`./post/${post.id}`}>
+                                    <PostPreview postData={post} key={post.id} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <PostWriter uploadPost={uploadPost} user={user} />
+            </div>
+        </>
+    )
+}
 
-export default MainPage;
+export default MainPage
