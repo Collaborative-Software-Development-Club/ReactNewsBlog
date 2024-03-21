0566cf2aa3a6d0b7952bde4f26226f85e2d63578
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-03-21 13:24:17 -0400

    final changes to char count

--- react-news-blog/src/layouts/MainPage/PostWriter.jsx
+++ react-news-blog/src/layouts/MainPage/PostWriter.jsx
@@ -1,12 +1,15 @@
 import React, { useRef } from 'react'
 import { useState } from 'react'
 
+const TITLE_LIMIT = 60
+
 const PostWriter = ({ uploadPost, user }) => {
     const titleRef = useRef()
     const contentRef = useRef()
 
     //set the titleCharCount to be set to the current length of the title to start with.
-    const[title, setTitle] = useState("");
+    const [title, setTitle] = useState('')
+    const titleLength = title.length
 
     const createPostData = (title, content) => {
         return {
@@ -20,35 +23,36 @@ const PostWriter = ({ uploadPost, user }) => {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-
         uploadPost(createPostData(titleRef.current.value, contentRef.current.value))
-        
     }
 
-
     //create a boolean for the limit of characters then use attribute disabled on the submit button when
-    //we do not have an appropriate character count!
-    let titleIsValid = title.length > 0 && title.length <= 30;
-    
+    const titleIsTooLong = titleLength > TITLE_LIMIT
+    const titleIsEmpty = titleLength == 0
+    const titleIsValid = !titleIsTooLong && !titleIsEmpty
 
     return (
-        <div>
+        <form onSubmit={handleSubmit} className="post-creation-form">
             <h2>Write a post</h2>
-            <form onSubmit={handleSubmit} className="post-creation-form">
-                <p>Posting as {user.name}</p>
-                <label htmlFor='title'>Title</label>
-                <input type='text' name='title' ref={titleRef} 
-                     onChange={(e) => setTitle(e.target.value)}/>
-                <label>Title char count: {title.length}</label>
-                <label htmlFor='content'>Content:</label>
-                <textarea ref={contentRef} name='content'></textarea>
-                <input type='submit' value='submit' disabled = {!titleIsValid}></input>
-                {
-                    titleIsValid || <p>The title must be under 31 characters and greater than 0.</p> 
-                }
-                
-            </form>
-        </div>
+            <p>Posting as {user.name}</p>
+            <label htmlFor="title">Title</label>
+            <input
+                type="text"
+                name="title"
+                ref={titleRef}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <p className={!titleIsValid && 'error'}>
+                ({title.length}/{TITLE_LIMIT})
+            </p>
+            {titleIsTooLong && (
+                <p className="error">The title must no longer than {TITLE_LIMIT} characters.</p>
+            )}
+            {titleIsEmpty && <p className="error">Title can't be empty.</p>}
+            <label htmlFor="content">Content:</label>
+            <textarea ref={contentRef} name="content"></textarea>
+            <input type="submit" value="submit" disabled={!titleIsValid}></input>
+        </form>
     )
 }
 
